3fa50cc9394eaf4a405e28f856a3ba2f7c48aa84
Kushal Manikonda <kushalmanikonda@gmail.com>
2025-12-18 22:40:24 +0530

    Update CartMapper.java

--- src/main/java/com/ecobazaarx/backend/mapper/CartMapper.java
+++ src/main/java/com/ecobazaarx/backend/mapper/CartMapper.java
@@ -4,7 +4,6 @@ import com.ecobazaarx.backend.dto.CartItemDTO;
 import com.ecobazaarx.backend.dto.CartResponseDTO;
 import com.ecobazaarx.backend.entity.Cart;
 import com.ecobazaarx.backend.entity.CartItem;
-import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -12,10 +11,16 @@ import java.util.List;
 @Component
 public class CartMapper {
 
+    /**
+     * Convert CartItem -> CartItemDTO
+     */
     public CartItemDTO toItemDTO(CartItem item) {
 
-        double totalPrice = item.getQuantity() * item.getProduct().getPrice();
-        double totalCarbon = item.getQuantity() * item.getProduct().getCarbonImpactKg();
+        double totalItemPrice =
+                item.getQuantity() * item.getProduct().getPrice();
+
+        double totalItemCarbon =
+                item.getQuantity() * item.getProduct().getCarbonImpactKg();
 
         return CartItemDTO.builder()
                 .productId(item.getProduct().getId())
@@ -23,14 +28,15 @@ public class CartMapper {
                 .price(item.getProduct().getPrice())
                 .quantity(item.getQuantity())
                 .carbonImpactKg(item.getProduct().getCarbonImpactKg())
-                .totalItemPrice(totalPrice)
-                .totalItemCarbon(totalCarbon)
+                .totalItemPrice(totalItemPrice)
+                .totalItemCarbon(totalItemCarbon)
                 .build();
     }
 
-    public @Nullable CartResponseDTO toCartResponseDTO(Cart cart, List<CartItem> items) {
-
-        if (cart == null) return null;
+    /**
+     * Convert Cart + CartItems -> CartResponseDTO
+     */
+    public CartResponseDTO toCartResponseDTO(Cart cart, List<CartItem> items) {
 
         List<CartItemDTO> itemDTOs = items.stream()
                 .map(this::toItemDTO)
@@ -47,7 +53,7 @@ public class CartMapper {
         return CartResponseDTO.builder()
                 .cartId(cart.getId())
                 .userId(cart.getUser().getId())
-                //.items(itemDTOs)
+                .items(itemDTOs)
                 .totalPrice(totalPrice)
                 .totalCarbon(totalCarbon)
                 .build();
